f1e8db97addf04d0bd079c4a5ce7d0ca61d1185c
Joice W <joicewong@users.noreply.github.com>
2015-06-16 14:44:18 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  s <- NULL
+  set <- function(y){
+    x <<-y
+    s <<-NULL
+  }
+  get <- function ()x
+  setsd <- function(sd) s<<-sd
+  getsd <- function() s
+  list (set=set, get=get, setsd=setsd, getsd=getsd)
+} 
 
-}
 
-
-## Write a short comment describing this function
+## Storing values and get the standard deviation of the values
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  s <- x$getsd()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- sd(data)
+  x$setsd(s)
+  s
 }
